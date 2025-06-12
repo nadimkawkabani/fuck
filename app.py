29995d097ad5d910378c7421a3b90b1e290d6ec4
nadimkawkabani <nadim_kawkabani@hotmail.com>
2025-06-12 17:22:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.express as px  # <--- THIS IS THE NEW LINE
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix
