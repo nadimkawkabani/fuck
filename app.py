f88c7b843d3d8d68d86ff460ed446b7d74ea3a08
nadimkawkabani <nadim_kawkabani@hotmail.com>
2025-06-20 16:05:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -118,7 +118,7 @@ def plot_correlation_matrix(df, columns):
 
 # --- EDA Dashboard Function ---
 def display_eda_dashboard(df):
-    st.title("🏥 Unveiling Sepsis Mortality: An Exploratory Analysis of Clinical Risk Factors")
+    st.title("🏥 Unveiling Sepsis Mortality: An Exploratory Analysis of Clinical Risk Factors.")
     st.markdown("A deep dive into the sepsis patient dataset with interactive visualizations.")
     st.sidebar.header("🔍 EDA Filters")
     filtered_df = df.copy()
